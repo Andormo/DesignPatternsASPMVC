5d144deab9552645c34cb6355dcd17e71b57365b
Andormo <andormo@hotmail.com>
2023-04-20 22:09:05 -0300

    Primer Commit

---
+++ DesignPatternsASPMVC/Controllers/ProductDetailController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tools;
+using Tools.Earn;
+
+namespace DesignPatternsASPMVC.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+
+            IEarn localEarn = localEarnFactory.GetEarn();
+
+            ViewBag.TotalLocal = total + localEarn.Earn(total);
+
+            return View();
+        }
+    }
+}
